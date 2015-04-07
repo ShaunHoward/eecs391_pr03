531fc081a881ded8a2ce40fb1a933c2663eb4a43
ShaunHoward <smh150@case.edu>
2015-04-07 01:23:23 -0400

    Added bld peas action comments

--- src/edu/cwru/sepia/agent/planner/GameState.java
+++ src/edu/cwru/sepia/agent/planner/GameState.java
@@ -1,7 +1,6 @@
 package edu.cwru.sepia.agent.planner;
 
 import edu.cwru.sepia.agent.planner.actions.StripsAction;
-import edu.cwru.sepia.environment.model.state.State;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/edu/cwru/sepia/agent/planner/actions/BuildPeasantAction.java
+++ src/edu/cwru/sepia/agent/planner/actions/BuildPeasantAction.java
@@ -4,25 +4,51 @@ import edu.cwru.sepia.agent.planner.GameState;
 import edu.cwru.sepia.agent.planner.PlanPeasant;
 import edu.cwru.sepia.agent.planner.PlanResource;
 
+/**
+ * Builds a peasant action for use in the resource collection game
+ * of PS3 in EECS 391.
+ * 
+ * This action has certain preconditions that must be met
+ * in order to be made. These conditions are that there are fewer than 3 peasants
+ * already playing in the game (this is our global limit) and that the peasants
+ * have at least 400 gold collected.
+ * 
+ * The effects are that there is one more peasant added to the game and
+ * the peasants now have 400 less gold. The make span of this action is 1.
+ * 
+ * @author Shaun Howard
+ */
 public class BuildPeasantAction implements StripsAction {
 
-    /**
-    Build peasant
-        Preconditions: Gold >= 400 and population < 3
-        Effects: Gold -400 and add new peasant
-        Makespan: 1
-    **/
-
+	/**
+	 * Checks if the preconditions to build a peasant are met. 
+	 * Returns true if they are met.
+	 * @param currState - the current state
+	 * @param goalState - the goal state to reach
+	 * @return whether the current state can make a build peasant
+	 * action
+	 */
     @Override
-    public boolean preconditionsMet(GameState s, GameState goal) {
-        return s.gold >= 400 && s.peasants.size() < 3;
+    public boolean preconditionsMet(GameState currState, GameState goalState) {
+        return currState.gold >= 400 && currState.peasants.size() < 3;
     }
 
+    /**
+     * Applies this action to the given game state. 
+     * Returns the given game state with the effects of the
+     * action accounted for.
+     * @param s - the game state to apply the action to
+     * @return the given game state with the effects of the build
+     * peasant action accounted for
+     */
     @Override
     public GameState apply(GameState s) {
     	GameState newState = new GameState(s);
     	int nextID = 0;
     	
+    	//Determine the next id available for the new peasant.
+    	//This is based on the peasants and resources, collectively
+    	//that already exist on the map.
     	for (PlanPeasant peasant : newState.peasants){
     		if (peasant.id > nextID) {
     			nextID = peasant.id;
@@ -33,18 +59,34 @@ public class BuildPeasantAction implements StripsAction {
     			nextID = resource.id;
     		}
     	}
+    	
+    	//Get the next available id
     	nextID++;
+    	
+    	//create a new peasant with that id
         newState.peasants.add(new PlanPeasant(0, 0, 0, nextID));
+        
+        //account for making the peasant since it takes 400 gold
         newState.gold -= 400;
+        
+        //track the action that made the new state
         newState.parentAction = this;
         return newState;
     }
 
+    /**
+     * The make span of building a peasant is 1.
+     * @return the make span of building a peasant
+     */
     @Override
     public int getMakeSpan() {
         return 1;
     }
 
+    /**
+     * Returns the type of action as a string.
+     * @return the type of action as a string
+     */
     @Override
     public String toString() {
         return "BUILD_PEASANT()";
